2ed8726c4b10b819bb16dfba16b07a13219825a2
1lucascq <1lucascq@gmail.com>
2022-03-27 20:02:25 -0300

    doc: muda o nome da rota da documentacao para testar deploy

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerDoc))
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc))
 
 app.use("/", router);
 
