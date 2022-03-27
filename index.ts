5c69b98c1a93b6ec57ad46ae5aef4d37aa85452e
1lucascq <1lucascq@gmail.com>
2022-03-27 01:32:17 -0300

    refac: ajusta arquivos do index.ts

--- index.ts
+++ index.ts
@@ -1,16 +1,19 @@
-const express = require("express");
-const ErrorHandler = require("./middlewares/ErrorHandler");
-const PhBookController = require("./controllers/PhBookController");
+import express from 'express';
+import ErrorHandler from './middlewares/ErrorHandler';
+import router from './routes';
+import 'express-async-errors'
+
+
+
 const cors = require('cors');
-
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
+
 app.use(express.json());
 
-app.use("/contacts", PhBookController);
+app.use("/", router);
 
 app.use(ErrorHandler);
 
